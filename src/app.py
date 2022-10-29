51f4a0b7a6ffb8f8e732858eae18f23ec31b4fe0
Michael Nikolaev <esp12p@gmail.com>
2022-10-29 10:01:15 +0300

    22.10.29

--- src/app.py
+++ src/app.py
@@ -5,6 +5,5 @@ from sys import platform
 #SETUP
 print("Starting...")
 eel.init("web")
-eel.start("error_osnotupported.html",mode="chromium", port="",size=(100,100)) #cmdline_args=['--start-fullscreen']
-
+eel.start("error_osnotsupported.html",mode="chrome",size=(500,500), cmdline_args=['--start-fullscreen'])
 #MAIN

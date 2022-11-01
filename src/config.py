80d52e5b026d55ebc35864ddcddddc9360f5bcbf
Michael Nikolaev <esp12p@gmail.com>
2022-11-01 18:38:37 +0300

    22/11/1

--- src/config.py
+++ src/config.py
@@ -1,8 +1,13 @@
 # KEYBOARD
+
+
 keyboard_move_forward   = "w"
 keyboard_move_left      = "a"
 keyboard_move_right     = "d"
 keyboard_move_back      = "s"
 
-keyboard_reverse_roller = "q"
-keyboard_strike         = "[SPACE]"
+keyboard_reverse_roller = "r"
+keyboard_strike         = " "
+#BLUETOOTH
+bluetooth_mac           = "00:18:91:D7:4D:79"   #For Linux user
+bluetooth_port          = "COM5"                #For Windows user
--- src/main.py
+++ src/main.py
@@ -2,31 +2,56 @@
 ##### SETUP
 #####----------------------
 import sys
+import platform
 import os
 import keyboard
 import random
 import serial
 import config
+import time
 
+key_move_forward  = config.keyboard_move_forward
+key_move_left     = config.keyboard_move_left
+key_move_right    = config.keyboard_move_right
+key_move_back     = config.keyboard_move_back
+key_reverse_roller= config.keyboard_reverse_roller
+system = platform.system()
+move = [0,0]
 
-keyboard_move_forward  = config.keyboard_move_forward
-keyboard_move_left     = config.keyboard_move_left
-keyboard_move_right    = config.keyboard_move_right
-keyboard_move_back     = config.keyboard_move_back
-keyboard_reverse_roller= config.keyboard_reverse_roller
 #####----------------------
 ##### MAIN
 #####----------------------
-#   MOVE
-keyboard.add_hotkey(keyboard_move_forward, lambda:wasd(255,0)) #forward
-keyboard.add_hotkey(keyboard_move_left, lambda:wasd(0,-255)) #left
-keyboard.add_hotkey(keyboard_move_right, lambda:wasd(0,255)) #right
-keyboard.add_hotkey(keyboard_move_back, lambda:wasd(-255,0)) #back
-
-
-keyboard.add_hotkey(f"{keyboard_move_forward} + {keyboard_move_right}", lambda:wasd(255,255)) #forward+right
-
-
-def wasd(Y,X):
-    print(Y,X)
-keyboard.wait()
+while True:
+    #                                                               MOVE
+    keyboard.add_hotkey(f"{key_move_forward}",                  lambda:forward())       #HOT-KEY FORWARD
+    def forward():
+        move[0]=255
+    keyboard.add_hotkey(f"{key_move_back}",                     lambda:back())          #HOT-KEY BACK
+    def back():
+        move[0]=-255
+    keyboard.add_hotkey(f"{key_move_left}",                     lambda:left())          #HOT-KEY LEFT
+    def left():
+        move[1]=-255
+    keyboard.add_hotkey(f"{key_move_right}",                    lambda:right())         #HOT-KEY RIGHT
+    def right():
+        move[1]=255
+    keyboard.add_hotkey(f"{key_move_forward}+{key_move_left}",  lambda:forward_left())  #HOT-KEY FORWARD + LEFT
+    def forward_left():
+        move[0]=255
+        move[1]=-255
+    keyboard.add_hotkey(f"{key_move_forward}+{key_move_right}", lambda:forward_right()) #HOT-KEY FORWARD + RIGHT
+    def forward_right():
+        move[0]=255
+        move[1]=255
+    keyboard.add_hotkey(f"{key_move_back}+{key_move_left}",     lambda:back_left())     #HOT-KEY BACK + LEFT
+    def back_left():
+        move[0]=-255
+        move[1]=-255
+    keyboard.add_hotkey(f"{key_move_back}+{key_move_right}",    lambda:back_right)      #HOT-KEY BACK + RIGHT
+    def back_right():
+        move[0]=-255
+        move[1]=255
+    keyboard.add_hotkey(' ',                    lambda:print('hit'))                    #HOT-KEY HIT
+    print(f"Y: {move[0]} X: {move[1]}")
+    move=[0,0]
+    time.sleep(0.05)
